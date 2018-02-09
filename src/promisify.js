c8c427fbf9cf6d2e32d3bfa5859d168ef4e7213d
Michael Toth <mtoth@way2b1.com>
2018-02-09 13:09:35 -0800

    Fix promisfy deps to work with webpack

--- src/promisify.js
+++ src/promisify.js
@@ -1,6 +1,6 @@
 import pify from 'pify';
 
-module.exports = ['fs', 'mkdirp'].reduce((acc, x) => {
-  acc[x] = pify(require(x));
-  return acc;
-}, {});
+module.exports = {
+    fs: pify(require("fs")),
+    mkdirp: pify(require("mkdirp"))
+}
